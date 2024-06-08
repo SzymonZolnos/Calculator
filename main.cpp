cbe2e191d95b1bf1874979bb2f49216d7e4232ee
Szymon <szychu60@gmail.com>
2024-06-08 21:32:23 +0200

    Dodanie mnozenia

--- main.cpp
+++ main.cpp
@@ -38,6 +38,11 @@ int main() {
                 result = number1 - number2;
                 printf("%d - %d = %d\n", number1, number2, result);
                 break;
+            case 3:
+                enterNumbers(&number1, &number2);
+                result = number1 * number2;
+                printf("%d * %d = %d\n", number1, number2, result);
+                break;
             default:
                 printf("Choose the right option...\n\n");
         }
