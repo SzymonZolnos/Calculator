59843c89100c29b69a3bb0be52f457d9ec3db537
Szymon <szychu60@gmail.com>
2024-06-08 21:24:08 +0200

    wybieranieNumerow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
+void enterNumbers(int *number1, int *number2) {
+    printf("Enter first number: ");
+    scanf("%d", number1);
+    printf("Enter second number: ");
+    scanf("%d", number2);
+}
 
+int main() {
+    int a,b;
+    enterNumbers(&a,&b);
     return 0;
 }
